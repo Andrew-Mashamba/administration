69077e9470f475c6d7bc21d3e69fa41957e95149
Andrew Mashamba <andrew@zima.co.tz>
2025-05-19 18:26:01 +0300

    hfghgfgfgfg

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -10,16 +10,28 @@ class WelcomeEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public $data;
+    public $email;
+    public $password;
+    public $url;
+    public $name;
 
     public function __construct(array $data)
     {
-        $this->data = $data;
+        $this->email = $data['email'];
+        $this->password = $data['password'];
+        $this->url = $data['url'];
+        $this->name = $data['name'];
     }
 
     public function build()
     {
         return $this->view('emails.welcome')
-                    ->subject('Welcome to Your New SACCO Instance');
+                    ->subject('Welcome to Your New SACCO Instance')
+                    ->with([
+                        'email' => $this->email,
+                        'password' => $this->password,
+                        'url' => $this->url,
+                        'name' => $this->name
+                    ]);
     }
 } 
